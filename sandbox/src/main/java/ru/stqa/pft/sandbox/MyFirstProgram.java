0e77b0a40cdd0c044e4d976f3bfa8e55e6336e3b
Ivan Koshcheiev <koshcheiev.vanya@gmail.com>
2018-07-22 22:39:48 +0300

    First assert is written

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,23 +6,15 @@ public class MyFirstProgram {
 	    String smb = "somebody";
 		hello(smb);
 
-		double a = 5;
-        System.out.println("Площадь квадрата со стороной " + a + " = " + area(a));
+		Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.a + " = " + s.area());
 
 
-        double b = 6;
-        System.out.println("Площадь квадрата со стороными " + a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(5, 6);
+        System.out.println("Площадь квадрата со стороными " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String smb){
         System.out.println("Hello " + smb + "!");
-    }
-
-    public static double area(double a){
-	    return a * a;
-    }
-
-    public static double area(double a, double b){
-        return a * b;
     }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+    public double a;
+
+    Square(double a) {
+        this.a = a;
+    }
+
+    public double area(){
+        return a * a;
+    }
+}
