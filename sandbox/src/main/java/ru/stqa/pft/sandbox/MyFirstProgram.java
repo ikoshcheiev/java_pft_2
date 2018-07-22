9fd8732c973750e98ca367d5f9e797afb766551c
Ivan Koshcheiev <koshcheiev.vanya@gmail.com>
2018-07-22 21:56:05 +0300

    Added changes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args){
+	    String smb = "somebody";
+		hello(smb);
 
-		System.out.println("Hello world!");
-	}	
+		double a = 5;
+        System.out.println("Площадь квадрата со стороной " + a + " = " + area(a));
+
+
+        double b = 6;
+        System.out.println("Площадь квадрата со стороными " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String smb){
+        System.out.println("Hello " + smb + "!");
+    }
+
+    public static double area(double a){
+	    return a * a;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
+    }
 }
