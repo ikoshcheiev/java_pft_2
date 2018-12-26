ad39de888f562cac114cd30975ac7e696598c3b1
Ivan Koshcheiev <koshcheiev.vanya@gmail.com>
2018-12-26 23:46:24 +0200

    l4_m9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,13 +20,30 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);
 
         //l4_m8
-        int max = 0;
+        /*int max = 0;
         for (GroupData g : after) {
             if(g.getId() > max) {
                 max = g.getId();
             }
-        }
-        group.setId(max);
+        }*/
+
+        //l4_m9
+
+        //Comparator<? super GroupData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+        /*
+        Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+            @Override
+            public int compare(GroupData o1, GroupData o2) {
+                return Integer.compare(o1.getId(), o2.getId());
+            }
+        };
+        int max = after.stream().max(byId).get().getId();
+        */
+
+        //int max = after.stream().max(((o1, o2) -> Integer.compare(o1.getId(), o2.getId()))).get().getId();
+        //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        group.setId(after.stream().max((Comparator.comparingInt(GroupData::getId))).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
