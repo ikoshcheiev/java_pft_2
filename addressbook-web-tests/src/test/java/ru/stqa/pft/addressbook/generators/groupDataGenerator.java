41684705f873897786f8a28ddd9538bd29d83247
Ivan Koshcheiev <koshcheiev.vanya@gmail.com>
2019-01-11 01:09:13 +0200

    l6_m6 writing/reading xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/groupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/groupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -20,9 +21,11 @@ public class GroupDataGenerator {
     @Parameter(names = "-f", description = "Target file")
     public String file;
 
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
     public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
-        //new JCommander(generator, args);
         JCommander jCommander = new JCommander(generator);
         try{
             jCommander.parse(args);
@@ -30,20 +33,35 @@ public class GroupDataGenerator {
             jCommander.usage();
             return;
         }
-
         generator.run();
-        //int count = Integer.parseInt(args[0]);
-        //File file = new File(args[1]);
     }
 
     private void run() throws IOException {
-
         List<GroupData> groups = generateGroups(count);
-        save(groups, new File(file));
-
+        if(format.equals("csv")){
+            saveAsCSV(groups, new File(file));
+        }else if(format.equals("xml")){
+            saveAsXML(groups, new File(file));
+        }else {
+            System.out.println("Unrecognized format " + format);
+        }
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXML(List<GroupData> groups, File file) throws IOException {
+        // How to make XML saving on X-Stream - http://x-stream.github.io/tutorial.html
+        XStream xstream = new XStream();
+        // Make proper tag instead of <ru.stqa.pft.addressbook.model.GroupData>
+        //xstream.alias("group", GroupData.class);
+        // OR mention the annotation in class GroupData @XStreamAlias("group")
+        xstream.processAnnotations(GroupData.class);
+        // Make object to xml string
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCSV(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
@@ -57,8 +75,8 @@ public class GroupDataGenerator {
         for (int i = 0; i < count; i++) {
             groups.add(new GroupData()
                     .withName(String.format("test %s", i))
-                    .withHeader(String.format("header %s", i))
-                    .withFooter(String.format("footer %s", i)));
+                    .withHeader(String.format("header\n%s", i))
+                    .withFooter(String.format("footer\n%s", i)));
         }
         return groups;
     }
